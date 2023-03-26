00fbb8837b754a1912f190378d6cf1cd7c015d77
tushargaur3 <120725354+tushargaur3@users.noreply.github.com>
2023-03-26 15:33:43 +0530

    Update Pad.js

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,17 +2,22 @@ import React from "react";
 import {bank1} from "./App"
 
 function Pad({handleClick, power, backgroundStyle, element, id }) {
+    const backgroundStyle = power ? "orange" : "gray";
     return (
       <button 
         data-tag={id}
         type='button'
         className='drum-pad' 
         onClick={handleClick} 
-        id={bank1[element]} 
+        id={bank1[element].name} 
         disabled={!power} 
         style={{background: `${backgroundStyle}`}}>
         {element}
-        <audio id={element} src={bank1[element].source} className='clip'></audio>
+        <audio 
+        id={element} 
+        src={bank1[element].source} 
+        className='clip'>
+        </audio>
       </button>
     );
   }
